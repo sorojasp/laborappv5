9d6f97215f6eb261d044cd461822bec555fbe954
sorojasp <stivenorlandorojaspulido@gmail.com>
2021-09-18 16:49:02 -0500

    we add user model

--- laborapp/settings.py
+++ laborapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user_profile'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL='user_profile.UserProfile'
