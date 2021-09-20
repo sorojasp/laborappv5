fc272ef4581686b61c629dc3c8d650a19d59f0c8
sorojasp <stivenorlandorojaspulido@gmail.com>
2021-09-20 09:51:11 -0500

    test with taskApp

--- laborapp/urls.py
+++ laborapp/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from user import views as user_views
 from django.conf.urls.static import static
 from django.conf import settings
 
-router: DefaultRouter = DefaultRouter()
-router.register('', user_views.UserView, basename="")
+
 
 
 
--- laborapp/wsgi.py
+++ laborapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskcontroller.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'laborapp.settings')
 
 application = get_wsgi_application()
