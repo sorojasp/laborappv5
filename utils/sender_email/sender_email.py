35a11336a5f98acbaedf88098d13929267188e44
sorojasp <stivenorlandorojaspulido@gmail.com>
2021-11-05 20:43:52 -0500

    Only is missing the comments of the methods in the class SenderEmail

--- utils/sender_email/sender_email.py
+++ utils/sender_email/sender_email.py
@@ -46,12 +46,32 @@ class SenderEmail:
 
         self.__message=MIMEMultipart()
 
-    def set_email(self, subject:str)->None:
+    def set_email(self,
+                   subject:str,
+                   header:str,
+                   message:str)->None:
         """
         """
+        html = f"""\
+        <html>
+         <body>
+         <p>{header}</p>
+        <p>{message}<p>
+        <p>Visita nuestro sito web: <a href="http://www.ingnovatech.com/recyapp">RecyApp</a> </p>
+        Salvando nuestro mundo...
+        </p>
+        </body>
+        </html>
+        """
+        #part1 = MIMEText(text, "plain")
+        part2 = MIMEText(html, "html")
+
+
+
+
         self.__message["From"] = self.__sender_email
         self.__message["Subject"] = subject
-        self.__message.attach(MIMEText(self.__body, "plain"))
+        self.__message.attach(part2)
 
 
 
@@ -88,18 +108,24 @@ class SenderEmail:
 
     def send_email(self,  receiver_email:str)->bool:
 
-        print(self.__smtp_server, self.__port, self.__sender_email, self.__password_email, )
+        try:
 
-        self.__message["To"] = receiver_email
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(self.__smtp_server, self.__port, context=context) as server:
+            print(self.__smtp_server, self.__port, self.__sender_email, self.__password_email, )
 
-            server.login(self.__sender_email, self.__password_email)
-            server.sendmail(self.__sender_email, receiver_email, self.__text_obj)
+            self.__message["To"] = receiver_email
+            context = ssl.create_default_context()
+            with smtplib.SMTP_SSL(self.__smtp_server, self.__port, context=context) as server:
+
+                server.login(self.__sender_email, self.__password_email)
+                server.sendmail(self.__sender_email, receiver_email, self.__text_obj)
+                return True
+        except Exception as error :
+            print("Error meanwhile send the email")
+            return False
 
 
 s_email=SenderEmail()
-s_email.set_email("Helloooooo =)")
+s_email.set_email("Helloooooo =)", "Hi", "I hope all is well")
 
 if s_email.attach_file("../../tmp/demanda.pdf"):
     s_email.send_email("stivenorlandorojaspulido@gmail.com")
