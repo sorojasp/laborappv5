b6da1e4cd6fddf70c2342b5b657ef3a4d380b457
sorojasp <stivenorlandorojaspulido@gmail.com>
2021-11-08 17:12:39 -0500

    pdf generated, sended and deleted successfuly

--- archivoDemanda/views.py
+++ archivoDemanda/views.py
@@ -13,85 +13,88 @@ from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 from django.conf import settings
 
-#libraries to send email
-import email, smtplib, ssl
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 
 
+#import pdf generator
+from utils.pdfGenerator.pdfGenerator import PdfGenerator
 
+#import sender email
+from utils.sender_email.sender_email import SenderEmail
+
+
+## libraries to make a pdf
+
+from reportlab.lib.pagesizes import letter
+
+file_name:str="demanda"
+extension_file:str="pdf"
+
+import random
+import time
 class ArchivoDemandaView(APIView):
 
     def post(self,  request, *args, **kwargs):
 
+        status=200
+        response={
+            'result':None,
+            'data':None,
+            'detail':None
+            }
 
-        files = request.FILES.getlist('demanda')
-        print("files: ", files)
-        files[0]
+        try:
 
+            number_file=random.randint(0, 10000)
 
-        path = default_storage.save('tmp/demanda.pdf', ContentFile(files[0].read()))
-        tmp_file = os.path.join(settings.MEDIA_ROOT, path)
+            while os.path.exists(f"./{file_name}_{number_file}.{extension_file}"):
+                number_file=random.randint(0, 10000)
 
-
-
-        # send emails with attach
-
-
-        subject = "An email with attachment from Python"
-        body = "This is an email with attachment sent from Python"
-        sender_email = "recyappbeta1@gmail.com"
-        receiver_email = request.query_params.get('email')
-        password = "#Stiven19111985"
-
-        # Create a multipart message and set headers
-        message = MIMEMultipart()
-        message["From"] = sender_email
-        message["To"] = receiver_email
-        message["Subject"] = subject
-        #message["Bcc"] = receiver_email  # Recommended for mass emails
-
-        # Add body to email
-        message.attach(MIMEText(body, "plain"))
+            file_name_full:str=f"{file_name}_{number_file}.{extension_file}"
 
 
-        filename = "tmp/demanda.pdf"  # In same directory as script
-        #in production the path is = "tmp/demanda.pdf"
-        # in develop the path is = tmp\demanda.pdf
+            pdf_generator=PdfGenerator()
+            pdf_generator.set_features(
+                             file_name_full,
+                             letter,
+                             72,
+                             72,
+                             72,
+                             18)
+            pdf_generator.set_styles()
+            pdf_generator.generate_pdf()
 
-        # Open PDF file in binary mode
-        with open(filename, "rb") as attachment:
-            # Add file as application/octet-stream
-            # Email client can usually download this automatically as attachment
-            part = MIMEBase("application", "octet-stream")
-            part.set_payload(attachment.read())
 
-        # Encode file in ASCII characters to send by email
-        encoders.encode_base64(part)
 
-        # Add header as key/value pair to attachment part
-        part.add_header(
-                "Content-Disposition",
-                f"attachment; filename= {filename}",
-            )
+            s_email=SenderEmail()
+            s_email.set_email("Helloooooo =)", "Hi", "I hope all is well")
 
-        # Add attachment to message and convert message to string
-        message.attach(part)
-        text = message.as_string()
+            if s_email.attach_file(f"./{file_name_full}"):
+                s_email.send_email("stivenorlandorojaspulido@gmail.com")
 
-        # Log in to server using secure context and send email
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, text)
+            os.remove(file_name_full)
+
+            print("** Pdf generate successfully")
+            response['result']=True
+            response['detail']= "** Pdf generated and sended successfully"
+
+
+
+        except Exception as err:
+            print("** error to generate and send the pdf:  ",error)
+            response['result']=False
+            response['detail']= str(error)
+            status=500
 
 
 
 
 
 
-        return Response({
-                        "hi":"hello"
-        }, status=200)
+        return Response(response, status=status)
+
+
+
+"""
+path = default_storage.save('tmp/demanda.pdf', ContentFile(files[0].read()))
+tmp_file = os.path.join(settings.MEDIA_ROOT, path)
+"""
--- utils/pdfGenerator/pdfGenerator.py
+++ utils/pdfGenerator/pdfGenerator.py
@@ -20,90 +20,164 @@ from django.conf import settings
 import random
 
 
-doc = SimpleDocTemplate("form_letter.pdf",pagesize=letter,
-                        rightMargin=72,leftMargin=72,
-                        topMargin=72,bottomMargin=18)
 
 
+def put_info():
 
-page=[]
-styles=getSampleStyleSheet()
-styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+    text:str = f"""\
 
-
-def add_image(img):
-    im = Image(img, 2*inch, 2*inch)
-    page.append(im)
-
-
-def add_space():
-    page.append(Spacer(1, 12))
-
-
-def add_text(text, space=0):
-    if type(text)==list:
-        for f in text:
-            add_text(f)
-    else:
-        ptext = f'<font size="12">{text}</font>'
-        page.append(Paragraph(ptext, styles["Normal"]))
-        if space==1:
-            add_space()
-        add_space()
-
-def show(text):
-    "Prints all the lines in the text multiline string"
-    text = text.splitlines()
-    for line in text:
-        if ".png" in line:
-            add_image(line)
-        elif "ctime()" in line:
-            add_text(time.ctime())
-        else:
-            add_text(line)
-
-
-# =============================== The content =======================
-
-text = """
-python_logo.png
 time.ctime()
 
-Giovanni Gatto
-Via Leonardo Da Vinci
-tel. 335556566
+Señor.
+Juez civil de pequeñas causas -Laboral Bogotá (Repardo)
+E.S.D
+
+Referencia : Demanda
+Demandante : MARIELA CENEDY PINTO BECERRA
+Demandado : Politécnico Rechimbo
+
 
 Hello,
 This is a formal letter
     Thank you very much and we look forward to serving you.
+
+Once upon a time there was an old mother pig who had three little pigs and not enough food to feed them. So when they were old enough, she sent them out into the world to seek their fortunes.
+The first little pig was very lazy. He didn't want to work at all and he built his house out of straw. The second little pig worked a little bit harder but he was somewhat lazy too and he built his house out of sticks. Then, they sang and danced and played together the rest of the day.
+The third little pig worked hard all day and built his house with bricks. It was a sturdy house complete with a fine fireplace and chimney. It looked like it could withstand the strongest winds.
+
+The next day, a wolf happened to pass by the lane where the three little pigs lived; and he saw the straw house, and he smelled the pig inside. He thought the pig would make a mighty fine meal and his mouth began to water.
+So he knocked on the door and said:
+
+So he huffed and he puffed and he blew the house down! The wolf opened his jaws very wide and bit down as hard as he could, but the first little pig escaped and ran away to hide with the second little pig.
+The wolf continued down the lane and he passed by the second house made of sticks; and he saw the house, and he smelled the pigs inside, and his mouth began to water as he thought about the fine dinner they would make.
+So he knocked on the door and said:
+
+Once upon a time there was an old mother pig who had three little pigs and not enough food to feed them. So when they were old enough, she sent them out into the world to seek their fortunes.
+The first little pig was very lazy. He didn't want to work at all and he built his house out of straw. The second little pig worked a little bit harder but he was somewhat lazy too and he built his house out of sticks. Then, they sang and danced and played together the rest of the day.
+The third little pig worked hard all day and built his house with bricks. It was a sturdy house complete with a fine fireplace and chimney. It looked like it could withstand the strongest winds.
+
+The next day, a wolf happened to pass by the lane where the three little pigs lived; and he saw the straw house, and he smelled the pig inside. He thought the pig would make a mighty fine meal and his mouth began to water.
+So he knocked on the door and said:
+
+So he huffed and he puffed and he blew the house down! The wolf opened his jaws very wide and bit down as hard as he could, but the first little pig escaped and ran away to hide with the second little pig.
+The wolf continued down the lane and he passed by the second house made of sticks; and he saw the house, and he smelled the pigs inside, and his mouth began to water as he thought about the fine dinner they would make.
+So he knocked on the door and said:
+
+Once upon a time there was an old mother pig who had three little pigs and not enough food to feed them. So when they were old enough, she sent them out into the world to seek their fortunes.
+The first little pig was very lazy. He didn't want to work at all and he built his house out of straw. The second little pig worked a little bit harder but he was somewhat lazy too and he built his house out of sticks. Then, they sang and danced and played together the rest of the day.
+The third little pig worked hard all day and built his house with bricks. It was a sturdy house complete with a fine fireplace and chimney. It looked like it could withstand the strongest winds.
+
+The next day, a wolf happened to pass by the lane where the three little pigs lived; and he saw the straw house, and he smelled the pig inside. He thought the pig would make a mighty fine meal and his mouth began to water.
+So he knocked on the door and said:
+
+So he huffed and he puffed and he blew the house down! The wolf opened his jaws very wide and bit down as hard as he could, but the first little pig escaped and ran away to hide with the second little pig.
+The wolf continued down the lane and he passed by the second house made of sticks; and he saw the house, and he smelled the pigs inside, and his mouth began to water as he thought about the fine dinner they would make.
+So he knocked on the door and said:
+
+    """
+    return text
+
+
+class PdfGenerator:
+
+    __doc:None
+    __page:list=[]
+    __styles:None
+
+    def __init__(self):
+        pass
+
+    def set_features(self,
+                     name_pdf_file:str,
+                     pagesize:str,
+                     rightMargin:int,
+                     leftMargin:int,
+                     topMargin:int,
+                     bottomMargin:int):
+        self.__doc = SimpleDocTemplate(name_pdf_file,pagesize=pagesize,
+                                rightMargin=rightMargin,leftMargin=leftMargin,
+                                topMargin=topMargin,bottomMargin=bottomMargin)
+
+    def set_styles(self, style_text='Justify'):
+        self.__styles=getSampleStyleSheet()
+        self.__styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+
+    def add_text(self, text:str, space=0):
+        """
+        Add text to the pdf line by line
+        """
+
+        if type(text)==list:
+            for f in text:
+                self.add_text(f)
+        else:
+            ptext = f'<font size="12">{text}</font>'
+            self.__page.append(Paragraph(ptext, self.__styles["Normal"]))
+            if space==1:
+                self.add_space()
+            self.add_space()
+
+    def add_space(self, ):
+        self.__page.append(Spacer(1, 12))
+
+    def show(self, text:str):
+        "Prints all the lines in the text multiline string"
+        text = text.splitlines()
+        for line in text:
+            if ".png" in line:
+                self.add_image(line)
+            elif "ctime()" in line:
+                self.add_text(time.ctime())
+            else:
+                self.add_text(line)
+
+    def add_image(self, img):
+        im = Image(img, 2*inch, 2*inch)
+        self.__page.append(im)
+
+    def show(self, text:str):
+        "Prints all the lines in the text multiline string"
+        text = text.splitlines()
+        for line in text:
+            if ".png" in line:
+                self.add_image(line)
+            elif "ctime()" in line:
+                self.add_text(time.ctime())
+            else:
+                self.add_text(line)
+
+
+
+    def show(self, text):
+        "Prints all the lines in the text multiline string"
+        text = text.splitlines()
+        for line in text:
+            if ".png" in line:
+                self.add_image(line)
+            elif "ctime()" in line:
+                self.add_text(time.ctime())
+            else:
+                self.add_text(line)
+
+    def generate_pdf(self):
+        self.show(put_info())
+        self.__doc.build(self.__page)
+
+
+"""
+# to make a test
+pdf_generator=PdfGenerator()
+pdf_generator.set_features(
+                 "demanda_48",
+                 letter,
+                 72,
+                 72,
+                 72,
+                 18)
+pdf_generator.set_styles()
+pdf_generator.generate_pdf()
 """
 
-
-
-# ===========================================================
-
-show(text)
-doc.build(page)
-os.system("echo form_letter.pdf")
-
-
-
-def set_name_file(file_name:str)->bool:
-    """Generate a number to add a namefile, if this
-       combination exists, then save the file: but if not exist
-       then generate a number until the file not exists to save it
-     """
-
-    number_file=random.randint(0, 10000)
-
-    print(os.path.exists(f"../../tmp/demanda_{number_file}.pdf"))
-
-
-    while os.path.exists(f"../../tmp/demanda_{number_file}.pdf"):
-        number_file=random.randint(0, 9)
-        print("gg", number_file)
-
-    print("path_file", f"../../tmp/demanda_{number_file}.pdf")
-
-
-set_name_file("")
+# start to save the file
+#path = default_storage.save('tmp/demanda.pdf', ContentFile(files[0].read()))
+#tmp_file = os.path.join(settings.MEDIA_ROOT, path)
