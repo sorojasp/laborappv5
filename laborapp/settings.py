2db60cc552e17c2c7c9562ee0834deb16b3d8510
sorojasp <stivenorlandorojaspulido@gmail.com>
2022-02-23 07:37:29 -0500

    fix database

--- laborapp/settings.py
+++ laborapp/settings.py
@@ -141,16 +141,14 @@ WSGI_APPLICATION = 'laborapp.wsgi.application'
 
 DATABASES = {
     'default': {
+    'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ingnova1_prueba',
+        'USER': 'ingnova1_stiven',
+        'PASSWORD': '#Stiven1911',
+        'HOST': '199.79.62.144',
+        'PORT': '3306',
 
-        'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'ingnova1_prueba',
-            'USER': 'ingnova1_stiven',
-            'PASSWORD': '#Stiven1911',
-            'HOST': '199.79.62.144',
-            'PORT': '3306',
-
-            }
-    }
+        }
 }
 
 
